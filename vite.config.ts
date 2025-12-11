8aa518b43af8a1fe4268c885e6c6913c29197c51
david-work-code <david2.hyun.song@gmail.com>
2025-12-11 09:46:04 -0500

    Set base path for Vite configuration
    base path to have my repository name

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/competitiveprice-100ae43e/',
   server: {
     host: "::",
     port: 8080,
